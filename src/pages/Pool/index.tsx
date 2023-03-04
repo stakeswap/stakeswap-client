a445b403f41b1912fc0f376bade06e8fbd74b461
smhwang0109 <smhwang0109@gmail.com>
2023-03-05 08:09:47 +0900

    Feat: Check wallet connect state & balance

--- src/pages/Pool/index.tsx
+++ src/pages/Pool/index.tsx
@@ -1,13 +1,14 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import React, { useEffect, useState } from 'react';
-import { Typography } from '@mui/material';
+import { debounce, Typography } from '@mui/material';
 import { useHistory } from 'react-router-dom';
-// import WalletIcon from '@mui/icons-material/Wallet';
+import WalletIcon from '@mui/icons-material/Wallet';
+import HighlightAltIcon from '@mui/icons-material/HighlightAlt';
 import { useAtom } from 'jotai';
 import { formatUnits } from 'ethers/lib/utils';
-import { BigNumber, FixedNumber, Signature } from 'ethers';
+import { BigNumber, ethers, FixedNumber, Signature } from 'ethers';
 import {
-  // GradientContainedButton,
+  GradientContainedButton,
   PrimaryContainedButton,
 } from '../../components/util/button';
 import TOKENLIST from '../../resources/token-list.json';
@@ -33,6 +34,7 @@ import {
   write__stakingPermitSigLocalStorageAtom,
   unstakingDataAtom,
   stakingPermitSigLocalStorageAtom,
+  providerAtom,
 } from '../../contracts';
 import { ERC20__factory } from '../../typechain';
 import PoolBackground from '../../assets/backgrounds/pool.png';
@@ -43,6 +45,7 @@ function Pool() {
   const [fromToken] = useAtom(fromTokenAtom);
   const [toToken, setToToken] = useAtom(toTokenAtom);
   const [signer] = useAtom(signerAtom);
+  const [provider, setProvider] = useAtom(providerAtom);
   const [signerAddress] = useAtom(signerAddressAtom);
   const [fromTokenState] = useAtom(fromTokenStateAtom);
   const [toTokenState] = useAtom(toTokenStateAtom);
@@ -96,6 +99,56 @@ function Pool() {
     stakingPermitSigLocalStorage,
     connected,
   ]);
+
+  const handleConnectWallet = async () => {
+    const newProvider = new ethers.providers.Web3Provider(
+      window.ethereum,
+      'any',
+    );
+    await newProvider.send('eth_requestAccounts', []);
+
+    try {
+      await window.ethereum.request({
+        method: 'wallet_switchEthereumChain',
+        params: [{ chainId: '0x760' }], // 0x760 = 1888 = forked testnet
+      });
+    } catch (switchErr: any) {
+      if (switchErr.code === 4902) {
+        try {
+          await window.ethereum.request({
+            method: 'wallet_addEthereumChain',
+            params: [
+              {
+                chainId: '0x760',
+                chainName: 'StakeSwap - forked testnet',
+                nativeCurrency: {
+                  name: 'tETH',
+                  symbol: 'tETH',
+                  decimals: 18,
+                },
+                rpcUrls: ['http://localhost:1888'], // mainnet-forked localhost testrpc
+                // blockExplorerUrls: [],
+              },
+            ],
+          });
+        } catch (addErr) {
+          console.log(addErr);
+        }
+      }
+      console.log(switchErr);
+    }
+
+    setProvider(newProvider);
+
+    const updater = debounce(() => {
+      if (toToken) setToToken(toToken);
+      if (stakingState) setStakingState(stakingState);
+    }, 4000);
+
+    newProvider.on('block', (blockNumber) => {
+      updater();
+    });
+  };
 
   const totalLPAmount =
     !connected || stakingState.totalSupply.eq(0)
@@ -155,16 +208,20 @@ function Pool() {
           }}
         >
           <Typography style={{ fontSize: '28px' }}>Pools</Typography>
-          <PrimaryContainedButton
-            width="150px"
-            height="40px"
-            fontSize="18px"
-            text="+ New Liquidity"
-            borderRadius="12px"
-            onClick={() => {
-              history.push('/pools/add');
-            }}
-          />
+          {!signerAddress ? (
+            <div />
+          ) : (
+            <PrimaryContainedButton
+              width="150px"
+              height="40px"
+              fontSize="18px"
+              text="+ New Liquidity"
+              borderRadius="12px"
+              onClick={() => {
+                history.push('/pools/add');
+              }}
+            />
+          )}
         </div>
         <div
           style={{
@@ -175,129 +232,77 @@ function Pool() {
             boxSizing: 'border-box',
           }}
         >
-          {/* <div
-          style={{
-            width: '100%',
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-          }}
-        >
-          <WalletIcon
-            style={{ width: '40px', height: 'auto', marginBottom: '20px' }}
-          />
-          <Typography
-            style={{
-              width: '300px',
-              marginBottom: '40px',
-              textAlign: 'center',
-              fontSize: '18px',
-            }}
-          >
-            Please connect your wallet.
-          </Typography>
-          <GradientContainedButton
-            width="300px"
-            height="40px"
-            fontSize="18px"
-            text="Connect a wallet"
-            borderRadius="24px"
-            onClick=""
-          />
-        </div> */}
-          <div
-            style={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              flexDirection: 'column',
-              alignItems: 'center',
-            }}
-          >
-            <Typography
-              style={{
-                width: '100%',
-                marginBottom: '30px',
-                fontSize: '16px',
-              }}
-            >
-              Your Position (1)
-            </Typography>
+          {!signerAddress ? (
             <div
               style={{
-                width: '530px',
-                padding: '18px 40px',
-                backgroundColor: secondary,
-                borderRadius: '16px',
-                boxSizing: 'border-box',
-                cursor: 'pointer',
+                width: '100%',
+                height: '100%',
+                padding: '40px 0',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
               }}
-              onClick={() => history.push('/pools/1')}
             >
-              <div
+              <WalletIcon
+                style={{ width: '40px', height: 'auto', marginBottom: '20px' }}
+              />
+              <Typography
                 style={{
-                  marginBottom: '28px',
-                  display: 'flex',
-                  alignItems: 'center',
+                  width: '300px',
+                  marginBottom: '40px',
+                  textAlign: 'center',
+                  fontSize: '18px',
                 }}
               >
-                <img
+                Please connect your wallet.
+              </Typography>
+              <GradientContainedButton
+                width="300px"
+                height="40px"
+                fontSize="18px"
+                text="Connect a wallet"
+                borderRadius="24px"
+                onClick={handleConnectWallet}
+              />
+            </div>
+          ) : !!lpTokenState && Number(lpTokenState.balance) > 0 ? (
+            <div
+              style={{
+                width: '100%',
+                height: '100%',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+              }}
+            >
+              <Typography
+                style={{
+                  width: '100%',
+                  marginBottom: '30px',
+                  fontSize: '16px',
+                }}
+              >
+                Your Position (1)
+              </Typography>
+              <div
+                style={{
+                  width: '530px',
+                  padding: '18px 40px',
+                  backgroundColor: secondary,
+                  borderRadius: '16px',
+                  boxSizing: 'border-box',
+                  cursor: 'pointer',
+                }}
+                onClick={() => history.push('/pools/1')}
+              >
+                <div
                   style={{
-                    width: '20px',
-                    height: '20px',
-                    marginRight: '-8px',
-                  }}
-                  src={fromTokenState?.logoURI ?? fromToken.logoURI}
-                  alt="token-logo"
-                />
-                <img
-                  style={{
-                    width: '20px',
-                    height: '20px',
-                    marginRight: '8px',
-                  }}
-                  src={toTokenState?.logoURI ?? toToken.logoURI}
-                  alt="token-logo"
-                />
-                <Typography
-                  style={{
-                    fontSize: '20px',
-                    fontWeight: 'bold',
+                    marginBottom: '28px',
+                    display: 'flex',
+                    alignItems: 'center',
                   }}
                 >
-                  {`${fromToken.symbol} / ${toToken.symbol}`}
-                </Typography>
-              </div>
-              <div
-                style={{
-                  marginBottom: '14px',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Typography style={{ fontSize: '16px' }}>
-                  Your total LP tokens:
-                </Typography>
-                <Typography style={{ fontSize: '16px' }}>
-                  {formatUnits(totalLPAmount, 18)}
-                </Typography>
-              </div>
-              <div
-                style={{
-                  marginBottom: '14px',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Typography style={{ fontSize: '16px' }}>
-                  Pooled {fromToken.symbol}:
-                </Typography>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography style={{ marginRight: '8px', fontSize: '16px' }}>
-                    {formatUnits(pooledETH, 18)}
-                  </Typography>
                   <img
                     style={{
                       width: '20px',
@@ -307,58 +312,133 @@ function Pool() {
                     src={fromTokenState?.logoURI ?? fromToken.logoURI}
                     alt="token-logo"
                   />
-                </div>
-              </div>
-              <div
-                style={{
-                  marginBottom: '14px',
-                  display: 'flex',
-                  justifyContent: 'space-between',
-                }}
-              >
-                <Typography style={{ fontSize: '16px' }}>
-                  Pooled {toToken.symbol}:
-                </Typography>
-                <div style={{ display: 'flex', alignItems: 'center' }}>
-                  <Typography style={{ marginRight: '8px', fontSize: '16px' }}>
-                    {formatUnits(pooledToken, tokenDecimals)}
-                  </Typography>
                   <img
                     style={{
                       width: '20px',
                       height: '20px',
-                      marginRight: '-8px',
+                      marginRight: '8px',
                     }}
                     src={toTokenState?.logoURI ?? toToken.logoURI}
                     alt="token-logo"
                   />
+                  <Typography
+                    style={{
+                      fontSize: '20px',
+                      fontWeight: 'bold',
+                    }}
+                  >
+                    {`${fromToken.symbol} / ${toToken.symbol}`}
+                  </Typography>
+                </div>
+                <div
+                  style={{
+                    marginBottom: '14px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Typography style={{ fontSize: '16px' }}>
+                    Your total LP tokens:
+                  </Typography>
+                  <Typography style={{ fontSize: '16px' }}>
+                    {formatUnits(totalLPAmount, 18)}
+                  </Typography>
+                </div>
+                <div
+                  style={{
+                    marginBottom: '14px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Typography style={{ fontSize: '16px' }}>
+                    Pooled {fromToken.symbol}:
+                  </Typography>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography
+                      style={{ marginRight: '8px', fontSize: '16px' }}
+                    >
+                      {formatUnits(pooledETH, 18)}
+                    </Typography>
+                    <img
+                      style={{
+                        width: '20px',
+                        height: '20px',
+                        marginRight: '-8px',
+                      }}
+                      src={fromTokenState?.logoURI ?? fromToken.logoURI}
+                      alt="token-logo"
+                    />
+                  </div>
+                </div>
+                <div
+                  style={{
+                    marginBottom: '14px',
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Typography style={{ fontSize: '16px' }}>
+                    Pooled {toToken.symbol}:
+                  </Typography>
+                  <div style={{ display: 'flex', alignItems: 'center' }}>
+                    <Typography
+                      style={{ marginRight: '8px', fontSize: '16px' }}
+                    >
+                      {formatUnits(pooledToken, tokenDecimals)}
+                    </Typography>
+                    <img
+                      style={{
+                        width: '20px',
+                        height: '20px',
+                        marginRight: '-8px',
+                      }}
+                      src={toTokenState?.logoURI ?? toToken.logoURI}
+                      alt="token-logo"
+                    />
+                  </div>
+                </div>
+                <div
+                  style={{
+                    display: 'flex',
+                    justifyContent: 'space-between',
+                  }}
+                >
+                  <Typography style={{ fontSize: '16px' }}>
+                    Your pool share:
+                  </Typography>
+                  <Typography style={{ fontSize: '16px' }}>
+                    {sharesPercent}%
+                  </Typography>
                 </div>
               </div>
-              <div
+            </div>
+          ) : (
+            <div
+              style={{
+                width: '100%',
+                height: '100%',
+                padding: '40px 0',
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+              }}
+            >
+              <HighlightAltIcon
+                style={{ width: '40px', height: 'auto', marginBottom: '20px' }}
+              />
+              <Typography
                 style={{
-                  display: 'flex',
-                  justifyContent: 'space-between',
+                  width: '300px',
+                  textAlign: 'center',
+                  fontSize: '18px',
                 }}
               >
-                <Typography style={{ fontSize: '16px' }}>
-                  Your pool share:
-                </Typography>
-                <Typography style={{ fontSize: '16px' }}>
-                  {sharesPercent}%
-                </Typography>
-              </div>
-              {/* <PrimaryContainedButton
-              width="100%"
-              height="38px"
-              fontSize="18px"
-              text="Remove"
-              borderRadius="12px"
-              onClick={() => {
-                history.push('pools/1/remove');
-              }}
-            /> */}
+                No liquidity found.
+              </Typography>
             </div>
-          </div>
+          )}
         </div>
       </div>
     </div>
